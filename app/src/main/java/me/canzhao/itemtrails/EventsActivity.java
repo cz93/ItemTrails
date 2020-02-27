6daa261c351e530418e9672e6c2c26e4a4d87bd4
cheterophylla <61527162+cheterophylla@users.noreply.github.com>
2020-02-27 23:34:09 +1300

    ...

--- app/src/main/java/me/canzhao/itemtrails/EventsActivity.java
+++ app/src/main/java/me/canzhao/itemtrails/EventsActivity.java
@@ -1,23 +1,32 @@
 package me.canzhao.itemtrails;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventsActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_events);
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+
+        ImageView itemImageView = findViewById(R.id.context_image);
+        itemImageView.setImageResource(R.drawable.outside_rugby_park);
+    }
+
+
+
+
 
     public String createArrivalNotice(String arrivalNotice) {
         boolean hasArrived = true;
-        if(hasArrived) {
+        if (hasArrived) {
             arrivalNotice = "You've arrived.";
         } else {
             arrivalNotice = "You're away from the place.";
         }
         return arrivalNotice;
     }
-
 }
